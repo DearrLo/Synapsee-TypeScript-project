426e9d77cb38ed37d3eed8135f0e35cdef4d4d6c
DearrLo <viellorette@gmail.com>
2025-09-05 00:23:24 +0200

    fixing

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,27 +1,26 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-// 🧪 Test temporaire : on hardcode l'URI
-const MONGO_URI = 'mongodb+srv://dearrlo:***REMOVED***@synapsee.sensmc3.mongodb.net/?retryWrites=true&w=majority&appName=Synapsee';
+// const MONGO_URI = 'URL';
 
-console.log("💥 DEBUG - MONGO_URI =", MONGO_URI);
+// console.log("DEBUG - MONGO_URI =", MONGO_URI);
 
-if (!MONGO_URI) {
-  throw new Error('MONGO_URI is not defined');
-}
+// if (!MONGO_URI) {
+//   throw new Error('MONGO_URI is not defined');
+// }
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+// let cached = (global as any).mongoose || { conn: null, promise: null };
 
-export async function connectToDatabase() {
-  if (cached.conn) return cached.conn;
+// export async function connectToDatabase() {
+//   if (cached.conn) return cached.conn;
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => {
-      console.log("✅ MongoDB connected!");
-      return mongoose;
-    });
-  }
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => {
+//       console.log("MongoDB connected!");
+//       return mongoose;
+//     });
+//   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
 
